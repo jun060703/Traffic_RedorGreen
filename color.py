111a3feded103cdfff7336c9daf37293476c63e7
kwonjun <116526991+jun060703@users.noreply.github.com>
2024-03-20 22:19:54 +0900

    color.py

--- color.py
+++ color.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-color = [0, 0, 255] # 파란색
+color = [0, 0, 255] # 빨간색
 pixel = np.uint8([[color]]) # 한픽셀로 구성된 이미지로 변환
 
 hsv = cv2.cvtColor(pixel, cv2.COLOR_BGR2HSV) 
